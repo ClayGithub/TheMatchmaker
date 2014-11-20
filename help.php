0feade8ceeeb0c50608b3dc239405b3e721c046d
ClayGithub <andrew.clay0435@gmail.com>
2014-11-19 21:31:19 -0700

    Almost completed Files
    Interface files almost completed will discuss thursday

---
+++ help.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<h2 style = 'text-align:center; font-size:50px;'>Matching Technology To People</h2>
+
+<p>
+	<a href='./index.php'>Matchmaker</a>
+	--
+	<a href='./results.php'>Results</a>
+	--
+	<a href='./about.php'>About</a>
+	--
+	<a href='./Help.php'>Help</a>
+</p>
+
+<style>
+table, th, td {
+     border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+
+
+
+<?php
+
+session_start();
+
+
+
+/*
+index.php
+Andrew Clay <andrew.c.clay@ucdenver.edu>
+File to view products
+
+
+*/
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
--- results.php
+++ results.php
@@ -5,7 +5,14 @@
 <h2 style = 'text-align:center; font-size:50px;'>Matching Technology To People</h2>
 
 <p>
-	<a href='./index.php'>Matchmaker</a></p>
+	<a href='./index.php'>Matchmaker</a>
+	--
+	<a href='./results.php'>Results</a>
+	--
+	<a href='./about.php'>About</a>
+	--
+	<a href='./Help.php'>Help</a>
+</p>
 
 <style>
 table, th, td {
@@ -33,7 +40,7 @@ File to view products
 
 error_reporting(E_ALL ^ E_NOTICE);
 // Create connection
-$con=mysqli_connect("","root","password","mydb");
+$conn= mysqli_connect("","root","password","matchmaker");
 
 // Check connection
 if (mysqli_connect_errno()) {
@@ -41,199 +48,586 @@ if (mysqli_connect_errno()) {
 }
 
 
-//Product information
-//Will Be replaced with database information
-/*
-$products = array(
-	1 => array(
-	'name' => 'Item#1',
-	'price' => 24.99,
-	'category' => 'Category#1',
-	'description' => 'Description 1'
-	),
-	2 => array(
-	'name' => 'Item#2',
-	'price' => 50.00,
-	'category' => 'Category#2',
-	'description' => 'Description 2'
-	),
-	3 => array(
-	'name' => 'Item#3',
-	'price' => 60000.50,
-	'category' => 'Category#3',
-	'description' => 'Description 3'
-	),
-	4 => array(
-	'name' => 'Item#4',
-	'price' => 24.99,
-	'category' => 'Category#4',
-	'description' => 'Description 4'
-	),
-	5 => array(
-	'name' => 'Item#5',
-	'price' => 50.00,
-	'category' => 'Category#5',
-	'description' => 'Description 5'
-	),
-	6 => array(
-	'name' => 'Item#6',
-	'price' => 60000.50,
-	'category' => 'Category#6',
-	'description' => 'Description 6'
-	)
-	
-	
-);
 
-
-
-
-
-
-if(isset($_GET['view_product'])) {
-	$product_id = $_GET['view_product'];
-	//View Individual product
-	
-	//Displays site links
-/*	echo "<p>
-	<a href='./index.php'>Matchmaker</a> &gt; <a href='./index.php'>" . 
-	$products[$product_id]['name'] . "</a></p>";
-	
-	
-	//Display products
-	echo "<p>
-		<span style='font-weight:bold;'>" . $products[$product_id]['name'] . "</span><br />
-		<span>$" . $products[$product_id]['price'] . "</span><br />
-		<span>" . $products[$product_id]['category'] . "</span><br />
-		<span>" . $products[$product_id]['description'] . "</span>
-	
-	</p>";
-	
-	
-	
+function individualProduct($idNumber) {
+    
 }
-else{
-	
-//
-//	echo "<p>
-//	<a href='./index.php'>Matchmaker</a></p>";
-	
-	
-	
-	
-	
-	echo "<h3 style = 'text-align:center;'>Products</h3>";
-	
-	echo "<table align='center' style='width:500px; text-align:center; cellspacing='0'>";
-	
-	echo "<tr style='border-bottom:1px solid #000000; text-align:center;'>
-		<th style='border-bottom:1px solid #000000;'>Name</th>
-		<th style='border-bottom:1px solid #000000;'>Price</th>
-		<th style='border-bottom:1px solid #000000;'>Category</th>
-	</tr>";
-
-	//Loop for displaying products
-
-	foreach($products as $id => $product) {
-		echo "<tr style = 'text-align:center;' >
-			<td style='border-bottom:1px solid #000000; text-align:center;'><a href='./index.php?view_product=$id'>" . $product['name'] . "</a></td>
-			<td style='border-bottom:1px solid #000000;'>$" . $product['price'] . "</td>
-			<td style='border-bottom:1px solid #000000;'>" . $product['category'] . "</td>
-		</tr>";
-
-		
-	}
-	echo "</table>";
-		
-}	
-
-
-*/
 
 $question1A = $_SESSION['question1'];
 $question2A = $_SESSION['question2'];
-$question3A = $_SESSION['question3'];
-$question4A = $_SESSION['question4'];
-
-if($question1A == "ans1")
-{
-	echo "<p>
-	$question1A
-	</p></ br>";
-}
-else if($question1A == "ans2")
-{
-	echo "<p>
-	$question1A
-	</p></ br>";
-}
-else if($question1A == "ans3")
-{
-	echo "<p>
-	$question1A
-	</p></ br>";
-}
-else if($question1A == "ans4")
-{
-	echo "<p>
-	$question1A
-	</p></ br>";
-}
-
-
-if($question2A == "type1")
-{
-	echo "<p>
-	$question2A
-	</p></ br>";
-}
-else if($question2A == "type2")
-{
-	echo "<p>
-	$question2A
-	</p></ br>";
-}
-else if($question2A == "type3")
-{
-	echo "<p>
-	$question2A
-	</p></ br>";
-}
-else if($question2A == "type4")
-{
-	echo "<p>
-	$question2A
-	</p></ br>";
-}
-
-if($question3A == "hearYes")
-{
-	echo "<p>
-	$question3A
-	</p></ br>";
-}
-else if($question3A == "hearNo")
-{
-	echo "<p>
-	$question3A
-	</p></ br>";
-}
-
-if($question4A == "seeYes")
-{
-	echo "<p>
-	$question4A
-	</p></ br>";
-}
-else if($question4A == "seeNo")
-{
-	echo "<p>
-	$question4A
-	</p></ br>";
-}
 
 
 
+if(isset($_GET['view_id'])) {
+	$product_id = $_GET['view_id'];
+	echo $product_id;
+	
+	$sql = "SELECT * FROM device WHERE device_id = " . $product_id;
+		
+			
+	$result = $conn-> query($sql);
+	
+	if ($result->num_rows > 0) {
+			echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+    // output data of each row
+			while($row = $result ->fetch_assoc()) {
+			
+				
+				
+				
+				
+				echo "<tr><td>". $row["device_name"]. "</td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+			}
+			echo "</table>";
+		} 
+		else {
+			echo "0 results";
+		}
+		
+		
+		
+	$conn->close();
+
+	
+}
+else{
+
+
+	if($question1A == "ans1")
+	{
+		echo "<p>
+		$question1A
+		</p></ br>";
+	}
+	else if($question1A == "ans2")
+	{
+		echo "<p>
+		$question1A
+		</p></ br>";
+	}
+	else if($question1A == "ans3")
+	{
+		echo "<p>
+		$question1A
+		</p></ br>";
+	}
+	else if($question1A == "ans4")
+	{
+		echo "<p>
+		$question1A
+		</p></ br>";
+	}
+
+	if($question2A == "type1")
+	{
+		echo "<p>
+		$question2A
+		</p></ br>";
+	}
+	else if($question2A == "type2")
+	{
+		echo "<p>
+		$question2A
+		</p></ br>";
+	}
+	else if($question2A == "type3")
+	{
+		echo "<p>
+		$question2A
+		</p></ br>";
+	}
+	else if($question2A == "type4")
+	{
+		echo "<p>
+		$question2A
+		</p></ br>";
+	}
+
+
+
+	if($question1A == "ans1")
+	{
+		if($question2A == "type1")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type2")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type3")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type4")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		
+	}
+	else if($question1A == "ans2")
+	{
+		if($question2A == "type1")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type2")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type3")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type4")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		
+	}
+	else if($question1A == "ans3")
+	{
+		if($question2A == "type1")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type2")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type3")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type4")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		
+	}
+	else if($question1A == "ans4")
+	{
+		if($question2A == "type1")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type2")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type3")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		else if($question2A == "type4")
+		{
+			$sql = "SELECT * FROM device";
+			
+				
+			$result = $conn-> query($sql);
+			
+			
+			if ($result->num_rows > 0) {
+				echo "<table align='center';><tr><th>Device Name</th><th>Description</th><th>Manufacturer</th></tr>";
+		// output data of each row
+				while($row = $result ->fetch_assoc()) {
+					$id = $row["device_id"];
+					
+					
+					
+					
+					echo "<tr><td><a href='./results.php?view_id=$id'>". $row["device_name"]. "</a></td><td>".$row["description"]."</td><td>".$row["manufacturer"]."</td></tr>";
+				}
+				echo "</table>";
+			} 
+			else {
+				echo "0 results";
+			}
+		
+		
+		
+			$conn->close();
+		}
+		
+	}
+	
+
+}
 ?>
 
 </body>
