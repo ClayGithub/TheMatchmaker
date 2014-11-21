34bee58b606c1f8a3c148814a9a4498a80cef032
autamata <autamata@users.noreply.github.com>
2014-11-20 17:22:36 -0700

    Update peripheral_device.sql

--- peripheral_device.sql
+++ peripheral_device.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 20, 2014 at 04:33 PM
+-- Generation Time: Nov 20, 2014 at 05:13 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.3.10-1ubuntu3.13
 
@@ -34,8 +34,9 @@ CREATE TABLE IF NOT EXISTS `peripheral_device` (
   `device_id` tinyint(3) unsigned NOT NULL,
   `entertainment` tinyint(1) NOT NULL,
   `everyday_task` tinyint(1) NOT NULL,
-  `communication_scheduling` tinyint(1) NOT NULL,
+  `communication` tinyint(1) NOT NULL,
   `website` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `scheduling` tinyint(1) NOT NULL,
   PRIMARY KEY (`peri_device_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -43,12 +44,11 @@ CREATE TABLE IF NOT EXISTS `peripheral_device` (
 -- Dumping data for table `peripheral_device`
 --
 
-INSERT INTO `peripheral_device` (`peri_device_id`, `compatibility`, `peri_device_name`, `price`, `device_id`, `entertainment`, `everyday_task`, `communication_scheduling`, `website`) VALUES
-(1, 0, 'hearing-aid helper', 10.99, 1, 0, 0, 0, ''),
-(2, 0, 'crutch', 30, 5, 0, 0, 0, ''),
-(3, 1, 'weelchair', 100, 3, 0, 0, 0, '');
+INSERT INTO `peripheral_device` (`peri_device_id`, `compatibility`, `peri_device_name`, `price`, `device_id`, `entertainment`, `everyday_task`, `communication`, `website`, `scheduling`) VALUES
+(1, 0, 'hearing-aid helper', 10.99, 1, 0, 0, 0, '', 0),
+(2, 0, 'crutch', 30, 5, 0, 0, 0, '', 0),
+(3, 1, 'weelchair', 100, 3, 0, 0, 0, '', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
