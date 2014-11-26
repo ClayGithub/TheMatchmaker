edf15f765067145aec9a23e47cb079f277e4c8bf
ClayGithub <andrew.clay0435@gmail.com>
2014-11-26 01:10:54 -0700

    Populated Database Files
    I added a bunch of devices.

--- device.sql
+++ device.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 20, 2014 at 04:37 PM
--- Server version: 5.5.38
--- PHP Version: 5.3.10-1ubuntu3.13
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2014 at 09:07 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -36,35 +36,46 @@ CREATE TABLE IF NOT EXISTS `device` (
   `price` double NOT NULL,
   `description` text COLLATE utf8_unicode_ci NOT NULL,
   `peri_device_id` tinyint(3) unsigned NOT NULL,
-  `adriod` tinyint(1) NOT NULL,
-  `apple` tinyint(1) NOT NULL,
-  `special` tinyint(1) NOT NULL,
+  `android` tinyint(4) NOT NULL,
+  `apple` tinyint(4) NOT NULL,
+  `specialized` tinyint(4) NOT NULL,
+  `entertainment` tinyint(4) NOT NULL,
+  `communication` tinyint(4) NOT NULL,
+  `everyday_tasks` tinyint(4) NOT NULL,
+  `scheduling` tinyint(4) NOT NULL,
   PRIMARY KEY (`device_id`),
   KEY `seller_id` (`seller_id`),
   KEY `disability_id` (`disability_id`),
   KEY `peri_device_id` (`peri_device_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `device`
 --
 
-INSERT INTO `device` (`device_id`, `device_name`, `disability_id`, `manufacturer`, `seller_id`, `compatibility`, `price`, `description`, `peri_device_id`, `adriod`, `apple`, `special`) VALUES
-(1, 'apple', 1, 'Apple', 1, 1, 700, 'white color, lightweight', 1, 0, 0, 0),
-(2, 'hearing aid', 3, 'New Ear Device', 3, 0, 99, 'light weight and power saving device', 3, 0, 0, 0),
-(3, 'deaf aid', 2, 'New Sign Language', 2, 1, 100, ' new face, light weight and power saving device', 2, 0, 0, 0);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `device`
---
-ALTER TABLE `device`
-  ADD CONSTRAINT `device_ibfk_1` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`seller_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `device_ibfk_2` FOREIGN KEY (`disability_id`) REFERENCES `disability` (`disability_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `device_ibfk_3` FOREIGN KEY (`peri_device_id`) REFERENCES `peripheral_device` (`peri_device_id`) ON UPDATE CASCADE;
+INSERT INTO `device` (`device_id`, `device_name`, `disability_id`, `manufacturer`, `seller_id`, `compatibility`, `price`, `description`, `peri_device_id`, `android`, `apple`, `specialized`, `entertainment`, `communication`, `everyday_tasks`, `scheduling`) VALUES
+(1, 'IPhone 6', 0, 'Apple', 0, 1, 299, 'Small in size with three screens size options.  Multiple memory sizes available as the price increases.  Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(2, 'IPad Air 2', 0, 'Apple', 0, 1, 499, 'Large touch screen tablet, communication abilities, price increases with memory size increase, Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(3, 'IPad Air', 0, 'Apple', 0, 1, 399, 'Large touch screen tablet, price increases as the memory increases. Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(4, 'IPad Mini', 0, 'Apple', 0, 1, 249, 'Smaller version of the IPad Air but larger touch screen tablet than an IPhone. Price increases as the memory increases. Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(5, 'IPad Mini 2', 0, 'Apple', 0, 1, 299, 'Smaller version of the IPad Air but larger touch screen tablet than an IPhone. Price increases as the memory increases. Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(6, 'IPad Mini 3', 0, 'Apple', 0, 1, 399, 'Smaller version of the IPad Air but larger touch screen tablet than an IPhone. Price increases as the memory increases. Numerous application compatibility through the ITunes network.', 0, 0, 1, 0, 1, 1, 1, 1),
+(7, 'Pronto Tech 7"', 0, 'Android', 0, 2, 49, '4 GB of memory, this device is compatible with all Android apps.  It has a double camera with a large 7" tablet touch screen and WiFi is available for this device.', 0, 1, 0, 0, 1, 1, 1, 1),
+(8, 'Megafeis Tablet', 0, 'Android', 0, 2, 79, '8 GB of memory, this device is compatible with all Android Applications.  It has the addition of a microphone for speaking to text messages, as well as has a large tablet touch screen.', 0, 1, 0, 0, 1, 1, 1, 1),
+(9, 'Digital Reins 9"', 0, 'Android', 0, 2, 179, 'A larger, 9" tablet touch screen, compatible with all Android Applications.  Has 8 GB of memory as well as a microphone for speaking to text messages', 0, 1, 0, 0, 1, 1, 1, 1),
+(10, 'iRulu 7"', 0, 'Android', 0, 2, 59, 'Android Applications compatibility with a 7" tablet touch screen.  Blue Tooth and Skype built in along with 4 GB of memory and has a built in MP3 player.', 0, 1, 0, 0, 1, 1, 1, 1),
+(11, 'Sky Devices Ski', 0, 'Android', 0, 2, 79, 'This is an Android phone with a larger touch screen,  it has 4 GB of ROM with an excellent microphone for speaking to text messages.', 0, 1, 0, 0, 1, 1, 1, 1),
+(12, 'Allora Keyboard', 0, 'Spectronics', 0, 3, 2495, 'Allora offers this keyboard for simple text to speech generating.  It comes with a waterproof keyboard and word prediction.', 0, 0, 0, 1, 0, 1, 0, 0),
+(13, 'Xpress', 0, 'Dynavox', 0, 4, 6514, 'Dynavox offers this systems which has Windows-based software as well as offers the user the ability to create sentences and conversations using pictures.', 0, 0, 0, 1, 0, 1, 1, 1),
+(14, 'Maestro', 0, 'Dynavox', 0, 5, 12049, 'The Maestro allows the user to create custom pages and allows speaking to text messages.  The device contains a camera, preloaded voices, is Microsoft compatible and has an eye-tracking accessory.', 0, 0, 0, 1, 0, 1, 1, 1),
+(15, 'Dynawrite', 0, 'Dynavox', 0, 6, 4995, 'Dynawrite is a small compact device that can record up to an hour of speaking and contains a computer keyboard for easy, familiar use.', 0, 0, 0, 1, 0, 1, 0, 0),
+(16, 'V+', 0, 'Dynavox', 0, 7, 6548, 'Windows-based device that has Integrated reader software and comes with Eye-tracking software.', 0, 0, 0, 1, 1, 1, 0, 1),
+(17, 'Lightwriter', 0, 'Toby Churchill', 0, 8, 6250, 'The Lightwriter receives texts, stores notes and can be used as a remote in the household.  This device has a large text display but does not speak.', 0, 0, 0, 1, 0, 1, 0, 0),
+(18, 'SL40 and Connect', 0, 'Toby Churchill', 0, 9, 3500, 'This is a larger version of the Lightwriter that has text and mobile phone abilities.  It can save large Microsoft document and has various voices available for text to speech.', 0, 0, 0, 1, 0, 1, 1, 1),
+(19, 'ECO2', 0, 'Prentke Romich', 0, 10, 7895, 'The ECO2 is a windows-based device with a large 14.1" screen and supports MP3 player files.  It has large keys, eye-tracking software and a built-in CD burner. ', 0, 0, 0, 1, 1, 1, 1, 1),
+(20, 'NOVA Chat 5', 0, 'Saltillo', 0, 11, 3195, 'This device has an Android platform and is the smallest of the NOVA series.  It comes with switch scanning and preloaded vocabulary.', 0, 0, 0, 1, 0, 1, 1, 1),
+(21, 'NOVA Chat 8', 0, 'Saltillo', 0, 12, 3970, 'This device begins with an Android platform it has many different vocabulary pages preloaded.', 0, 0, 0, 1, 0, 1, 1, 1),
+(22, 'NOVA Chat 10', 0, 'Saltillo', 0, 12, 4395, 'This device also has an Android platform as well as a speech synthesizer.  The NOVA Chat 10 allows the user to save files and has numerous voice options for the speaking feature.', 0, 0, 0, 1, 0, 1, 1, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- peripheral_device.sql
+++ peripheral_device.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Nov 20, 2014 at 05:13 PM
--- Server version: 5.5.38
--- PHP Version: 5.3.10-1ubuntu3.13
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2014 at 09:08 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -32,11 +32,11 @@ CREATE TABLE IF NOT EXISTS `peripheral_device` (
   `peri_device_name` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `price` double NOT NULL,
   `device_id` tinyint(3) unsigned NOT NULL,
-  `entertainment` tinyint(1) NOT NULL,
-  `everyday_task` tinyint(1) NOT NULL,
-  `communication` tinyint(1) NOT NULL,
-  `website` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `scheduling` tinyint(1) NOT NULL,
+  `entertainment` tinyint(4) NOT NULL,
+  `communication` tinyint(4) NOT NULL,
+  `scheduling` tinyint(4) NOT NULL,
+  `everyday_tasks` tinyint(4) NOT NULL,
+  `website` char(25) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`peri_device_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -44,10 +44,58 @@ CREATE TABLE IF NOT EXISTS `peripheral_device` (
 -- Dumping data for table `peripheral_device`
 --
 
-INSERT INTO `peripheral_device` (`peri_device_id`, `compatibility`, `peri_device_name`, `price`, `device_id`, `entertainment`, `everyday_task`, `communication`, `website`, `scheduling`) VALUES
-(1, 0, 'hearing-aid helper', 10.99, 1, 0, 0, 0, '', 0),
-(2, 0, 'crutch', 30, 5, 0, 0, 0, '', 0),
-(3, 1, 'weelchair', 100, 3, 0, 0, 0, '', 0);
+INSERT INTO `peripheral_device` (`peri_device_id`, `compatibility`, `peri_device_name`, `price`, `device_id`, `entertainment`, `communication`, `scheduling`, `everyday_tasks`, `website`) VALUES
+(1, 1, 'Proloquo2Go', 220, 1, 1, 1, 0, 0, 'Itunes'),
+(2, 1, 'Living Safely', 30, 1, 0, 0, 0, 1, 'Itunes'),
+(3, 1, 'iStudies Pro.', 3, 1, 0, 1, 1, 1, 'Itunes'),
+(4, 1, 'Conversation Builder Teen', 30, 1, 1, 1, 0, 0, 'Itunes'),
+(5, 1, 'Counting Bills and Coins', 0, 1, 1, 0, 0, 1, 'Itunes'),
+(6, 1, 'My Talk Tools', 100, 1, 1, 1, 0, 0, 'Itunes'),
+(7, 2, 'AAC Speech Buddy', 28, 2, 0, 1, 0, 0, 'Google Play'),
+(8, 2, 'Every Day To Do List Task List', 0, 2, 0, 0, 1, 1, 'Google Play'),
+(9, 2, 'Life Skills Winner', 0, 2, 1, 0, 0, 1, 'Google Play'),
+(10, 2, 'Smart Steps Mobile', 0, 2, 1, 1, 0, 0, 'Google Play'),
+(11, 2, 'JAB Talk ', 0, 2, 0, 1, 0, 0, 'Google Play'),
+(12, 2, 'Training Faces', 3, 2, 1, 1, 0, 0, 'Google Play'),
+(13, 3, 'Natural-Sounding Real Speak Vo', 0, 3, 0, 1, 0, 0, 'http://www.spectronics.co'),
+(14, 3, 'Word Prediction Software', 0, 3, 0, 1, 0, 0, 'http://www.spectronics.co'),
+(15, 3, 'Pre-Programmed Messages ', 0, 3, 0, 1, 0, 0, 'http://www.spectronics.co'),
+(16, 4, 'Series 5 Software w/ InterAACt', 0, 13, 1, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(17, 4, 'Picture Communication Symbols', 0, 13, 0, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(18, 4, 'AT&T Natural Voices ', 0, 13, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(19, 4, 'Acapela HQ Child and Adult Voi', 0, 13, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(20, 4, 'Loquendo Voices', 0, 13, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(21, 5, 'Page Editing Software', 0, 14, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(22, 5, 'Series 5 Software w/ InterAACt', 0, 14, 1, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(23, 5, 'Boardmaker w/ Speaking Dynamic', 0, 14, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(24, 5, 'Picture Communication Symbols', 0, 14, 0, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(25, 5, 'DynaSyms Symbols', 0, 14, 0, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(26, 5, 'Microsoft Voices', 0, 14, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(27, 6, 'Series 5 Software w/ InterAACt', 0, 15, 1, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(28, 7, 'Integrated Ebook Reader Softwa', 0, 16, 1, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(29, 7, 'Page Editing Software', 0, 16, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(30, 7, 'Series 5 Software w/ Interact', 0, 16, 1, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(31, 7, 'Eye Tracking System', 0, 16, 0, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(32, 8, 'Series 5 Software w/ InterAACt', 0, 17, 1, 1, 0, 1, 'http://www.dynavoxtech.co'),
+(33, 8, 'Acapela HQ Child and Adult Voi', 0, 17, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(34, 9, 'Acapela HQ Child and Adult Voi', 0, 18, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(35, 9, 'Series 5 Software w/ InterAACt', 0, 18, 1, 1, 0, 1, ''),
+(36, 9, 'Mobile Phone Connect Software', 0, 18, 0, 1, 0, 0, 'http://www.dynavoxtech.co'),
+(37, 10, 'Windows 7', 0, 19, 1, 1, 1, 1, 'https://store.prentrom.co'),
+(38, 10, 'Internal DVD/CD player/burner', 0, 19, 1, 1, 0, 0, 'https://store.prentrom.co'),
+(39, 10, 'ECO point eye gaze module', 0, 19, 0, 1, 1, 1, 'https://store.prentrom.co'),
+(40, 11, 'AACelerate with myVoice', 0, 20, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(41, 11, 'ChatPower', 0, 20, 1, 1, 0, 0, 'http://saltillo.com/produ'),
+(42, 11, 'Essence', 0, 20, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(43, 11, 'Vocab PC', 0, 20, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(44, 11, 'SymbolStix', 0, 20, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(45, 11, 'PCS', 0, 20, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(46, 12, 'Social Chat', 0, 21, 1, 1, 0, 0, 'http://saltillo.com/produ'),
+(47, 12, 'IVONA Speech Synthesizer', 0, 21, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(48, 12, 'AACelerate with myVoice', 0, 21, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(49, 12, 'ChatPower', 0, 21, 1, 1, 0, 0, 'http://saltillo.com/produ'),
+(50, 12, 'Essence', 0, 21, 0, 1, 0, 0, 'http://saltillo.com/produ'),
+(51, 12, 'Vocab PC', 0, 21, 0, 1, 0, 0, 'http://saltillo.com/produ');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
